9999aabc5ac6729e11ddc081245efb92f47384d5
Utilitycoder <66361605+Utilitycoder@users.noreply.github.com>
2022-01-04 19:27:16 +0100

    update staking

---
+++ staking.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.11;
+
+contract Storage {
+    function isActive() public view returns (bool)) {
+        
+    }
+
+    function getContractBalance() public view returns (uint) {
+
+    }
+
+    function deposit() public payable {
+        
+    }
+
+    receive() external payable {deposit();}
+
+    function withdraw() public {
+        
+    }
+
+}
